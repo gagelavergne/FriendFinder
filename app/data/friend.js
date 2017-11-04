9a6339bab615a57ac2e4879d37f3a8466e125516
Gage LaVergne <gagelavergne@Gages-MacBook-Pro.local>
2017-11-04 12:55:50 -0500

    added friends.js file, more to come

---
+++ app/data/friend.js
@@ -0,0 +1,99 @@
+var friends = [
+{
+	name: "Andrew",
+	photo: "https://homewardboundaz.org/wp-content/uploads/2016/09/person-placeholder.jpg",
+	scores: [
+		"4",
+		"3",
+		"1",
+		"2",
+		"1",
+		"3",
+		"2",
+		"2",
+		"3",
+		"1"
+	]
+},
+{
+    name: "Kendrick",
+    photo: "https://homewardboundaz.org/wp-content/uploads/2016/09/person-placeholder.jpg",
+    scores: [
+      "3",
+      "2",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "3",
+      "3",
+      "4"
+    ]
+  },
+  {
+    name: "Eddie",
+    photo: "https://homewardboundaz.org/wp-content/uploads/2016/09/person-placeholder.jpg",
+    scores: [
+      "3",
+      "2",
+      "4",
+      "4",
+      "4",
+      "1",
+      "3",
+      "1",
+      "5",
+      "3"
+    ]
+  },
+  {
+    name: "Hayden",
+    photo: "https://homewardboundaz.org/wp-content/uploads/2016/09/person-placeholder.jpg",
+    scores: [
+      "1",
+      "3",
+      "4",
+      "2",
+      "4",
+      "1",
+      "3",
+      "1",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Joel",
+    photo: "https://homewardboundaz.org/wp-content/uploads/2016/09/person-placeholder.jpg",
+    scores: [
+      "5",
+      "1",
+      "2",
+      "1",
+      "5",
+      "4",
+      "5",
+      "3",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: "Jack",
+    photo: "https://homewardboundaz.org/wp-content/uploads/2016/09/person-placeholder.jpg",
+    scores: [
+      "1",
+      "1",
+      "2",
+      "3",
+      "2",
+      "5",
+      "3",
+      "2",
+      "4",
+      "4"
+    ]
+  }
+
+]
